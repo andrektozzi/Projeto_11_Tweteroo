3580b240ad57fecec6be9b6d46e5fd7b10e9adae
andrektozzi <andrektozzi@gmail.com>
2022-06-16 20:32:04 -0300

    rota post sign up adicionada

--- index.js
+++ index.js
@@ -8,29 +8,10 @@ server.use(cors());
 const users = [];
 const tweets = [];
 
-server.post('/sign-up', (req, res) => {
+server.post("/sign-up", (req, res) => {
     const user = req.body
     users.push(user);
-    res.send('OK');
-});
-
-server.post('/tweets', (req, res) => {
-    const user = req.body
-    const tweet = {
-        username: user.username,
-        tweet: user.tweet
-    }
-    tweets.push(tweet);
-    res.send('OK');
-});
-
-server.get('/tweets', (req, res) => {
-    if(tweets.length < 10) {
-        res.send(tweets);
-    } else {
-        const lastTweets = tweets.slice(-10)
-        res.send(lastTweets);
-    }
+    res.send("OK");
 });
 
 server.listen(5000);
