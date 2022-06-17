1db12b3110c8f74ff19d0d856c1c2b6983448d10
andrektozzi <andrektozzi@gmail.com>
2022-06-16 21:05:10 -0300

    rota get tweets adicionada

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-const users = [];
-const tweets = [];
+const users = [
+    {
+        username: 'bobesponja', 
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+    }    
+];
+
+const tweets = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    }
+];
 
 server.post("/sign-up", (req, res) => {
     const user = req.body
@@ -15,13 +27,22 @@ server.post("/sign-up", (req, res) => {
 });
 
 server.post("/tweets", (req, res) => {
-    const user = req.body
-    const tweet = {
-        username: user.username,
-        tweet: user.tweet
-    }
-    tweets.push(tweet);
+    const user = {};
+    user.username = req.body.username;
+    user.avatar = users.find(user => user.username === req.body.username).avatar;
+    user.tweet = req.body.tweet;
+    tweets.push(user);
     res.send("OK");
-})
+});
+
+server.get("/tweets", (req, res) => {
+    let lastTweets;
+    if(tweets.length > 10) {
+        lastTweets = tweets.slice(tweets.length - 10);
+    } else {
+        lastTweets = tweets;
+    }
+    res.send(lastTweets);
+});
 
 server.listen(5000);
