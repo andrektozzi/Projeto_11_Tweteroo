3d220f6c93d4bbc2e369bebd19e037fd4641cc22
andrektozzi <andrektozzi@gmail.com>
2022-06-17 21:27:22 -0300

    rota post sign up corrigida

--- index.js
+++ index.js
@@ -5,44 +5,25 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-const users = [
-    {
-        username: 'bobesponja', 
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    }    
-];
-
-const tweets = [
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    }
-];
+const users = [];
+const tweets = [];
 
 server.post("/sign-up", (req, res) => {
-    const user = req.body
-    users.push(user);
+    const {user, avatar } = req.body
+    users.push({ user, avatar});
     res.send("OK");
 });
 
 server.post("/tweets", (req, res) => {
-    const user = {};
-    user.username = req.body.username;
-    user.avatar = users.find(user => user.username === req.body.username).avatar;
-    user.tweet = req.body.tweet;
-    tweets.push(user);
+    const userTweet = req.body
+    const infoTweet = {username: userTweet.username, avatar: users.avatar, tweet: userTweet.tweet}
+    tweets.push(infoTweet);
     res.send("OK");
 });
 
 server.get("/tweets", (req, res) => {
-    let lastTweets;
-    if(tweets.length > 10) {
-        lastTweets = tweets.slice(tweets.length - 10);
-    } else {
-        lastTweets = tweets;
-    }
-    res.send(lastTweets);
+    const lastTweets = tweets.reverse();
+    res.send(lastTweets.slice(0,10));
 });
 
 server.listen(5000);
